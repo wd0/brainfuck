ebf9e8e004047f3945276ba4cc2d20b11b0e8e42
wd0 <null@void>
2015-12-22 18:04:18 -0500

    Merge error module. Add manpage. Simplify main().

--- Makefile
+++ Makefile
@@ -1,14 +1,33 @@
 CC = cc
-SRC = brainfuck.c err.c
+SRC = brainfuck.c
+CFLAGS = -Wall -Wextra -ansi -pedantic 
+
 NAME = brainfuck
-CFLAGS = -Wall -Wpedantic -Wextra
+DOCNAME = ${NAME}.1.gz
 
+INSTALLPATH = /usr/local
+BINPATH = ${INSTALLPATH}/bin
+DOCPATH = ${INSTALLPATH}/share/man/man1
 
-all:	
+all: build doc
+
+build:
 	${CC} -o ${NAME} ${CFLAGS} ${SRC} 
 
 test: all
-	./${NAME} test/*.bf
+	! ./${NAME} test/*.bf
 
 clean:
 	rm -f ${NAME}
+	rm -f ${DOCNAME}
+
+doc:
+	gzip -c ${NAME}.1 >${DOCNAME}
+
+install: build doc
+	install -D ${NAME} ${BINPATH}
+	install -D ${DOCNAME} ${DOCPATH}/${DOCNAME} -m 0644
+
+remove:
+	rm -f ${BINPATH}/${NAME}
+	rm -f ${DOCPATH}/${DOCNAME}
--- brainfuck.c
+++ brainfuck.c
@@ -1,9 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "err.h"
+#include <errno.h>
+#include <stdarg.h>
+#include <string.h>
 
-enum { STACKSIZE = 30000 };
-enum { EXEC_MEMERR = -4, BF_ERR = -3, EXEC_ILL = -2, EXEC_DONE = -1 };
+/* Prints a warning plus a newline and then exits. */
+/* A warning ending in a colon will cause warn to append strerror() to the output. */
+/* If fmt is NULL then warn returns 1; else it returns 0. */
+int
+warn(const char *fmt, ...) {
+    va_list ap;
+    int len;
+    if (fmt == NULL) {
+        warn("%s", "warn: attempted to warn with null print format");
+        return 1;
+    }
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    len = strlen(fmt);
+    if (fmt[len-1] == ':') 
+        fprintf(stderr, " %s", strerror(errno));
+    putchar('\n');
+
+    return 0;
+}
+
+/* Warn and then exit. */
+void
+die(const char *fmt, ...) {
+    va_list ap;
+    int len;
+    if (fmt == NULL) {
+        fprintf(stderr, "%s", "die: attempted to die with null print format");
+        exit(1);
+    }
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    len = strlen(fmt);
+    if (fmt[len-1] == ':') 
+        fprintf(stderr, " %s", strerror(errno));
+    putchar('\n');
+
+    exit(1);
+}
+
+/* Malloc that never fails. Shows a warning and exits. */
+void *
+emalloc(size_t n) {
+    void *m = malloc(n);
+    if (!m)
+        die("emalloc(%z):", n);
+    return m;
+}
+
+enum {STACKSIZE = 30000};
+enum {EXEC_MEMERR = -4, BF_ERR = -3, EXEC_ILL = -2, EXEC_DONE = -1};
 
 /* Program flow: */
 /* load: A program is read completely in from a file. */
@@ -11,20 +66,20 @@ enum { EXEC_MEMERR = -4, BF_ERR = -3, EXEC_ILL = -2, EXEC_DONE = -1 };
 /* execute: A single instruction is executed, modifying the stack pointer,
    the stack, or the program counter. */
 
-const char *
+char *
 load(FILE *fin) {
-    enum { DEFAULT_TEXTSIZE = 128 };
+    enum {DEFAULT_TEXTSIZE = BUFSIZ};
     char *text = emalloc(DEFAULT_TEXTSIZE);
     size_t bufsize = DEFAULT_TEXTSIZE; 
     size_t i;
     int c;
 
     for (i = 0; (c = fgetc(fin)) != EOF; ++i) {
-	if (bufsize - 2 <= i) { 
-	    bufsize *= 2;
-	    text = realloc(text, bufsize);
-	}
-	text[i] = c;
+        if (bufsize - 2 <= i) { 
+            bufsize *= 2;
+            text = realloc(text, bufsize);
+        }
+        text[i] = c;
     }
     text[i] = '\0';
 
@@ -32,12 +87,12 @@ load(FILE *fin) {
 }
 
 char
-peek(char *sp) {
+peek(const char *sp) {
     return *sp;
 }
 
-/* Function pointers */
-typedef int (*Fcond)(int n);
+/* jmp's conditional function pointers */
+typedef int (*Jmpcond)(int n);
 
 int
 unconditional(int n) {
@@ -56,53 +111,53 @@ isnonzero(int n) {
 /* End function pointers */
 
 int
-jmp(const char *text, int *pc, char *sp, Fcond jmpcond) {
+jmp(const char *text, int *pc, char *sp, Jmpcond jmpcond) {
     int offset = 1; /* Start looking at the next instruction. */
     int nesting = 0;
     int status = 0;
     int i;
 
     if (!jmpcond(peek(sp)))
-	return status;
+        return status;
 
     switch (text[*pc]) {
-	/* Jump forwards to matching ']'. */
-	case '[':
-	    for (i = *pc + 1; text[i];  ++i, ++offset) {
-		if (text[i] == ']' && nesting == 0) {
-		    *pc += offset;
-		    break;
-		} else if (text[i] == ']') {
-		    --nesting;
-		} else if (text[i] == '[') {
-		    ++nesting;
-		} 
-	    }
-	    if (text[i] == '\0')
-		status = EXEC_ILL; /* No closing ']' */
-	    break; 
+        /* Jump forwards to matching ']'. */
+        case '[':
+            for (i = *pc + 1; text[i];  ++i, ++offset) {
+                if (text[i] == ']' && nesting == 0) {
+                    *pc += offset;
+                    break;
+                } else if (text[i] == ']') {
+                    --nesting;
+                } else if (text[i] == '[') {
+                    ++nesting;
+                } 
+            }
+            if (text[i] == '\0')
+                status = EXEC_ILL; /* No closing ']' */
+            break; 
 
-	/* Jump backwards to matching '['. */
-	case ']':
-	    for (i = *pc - 1; i >= 0; --i, ++offset) {
-		if (text[i] == '[' && nesting == 0) {
-		    *pc -= offset;
-		    break;
-		} else if (text[i] == '[') {
-		    --nesting;
-		} else if (text[i] == ']') {
-		    ++nesting;
-		} 
+            /* Jump backwards to matching '['. */
+        case ']':
+            for (i = *pc - 1; i >= 0; --i, ++offset) {
+                if (text[i] == '[' && nesting == 0) {
+                    *pc -= offset;
+                    break;
+                } else if (text[i] == '[') {
+                    --nesting;
+                } else if (text[i] == ']') {
+                    ++nesting;
+                } 
 
-	    }
-	    if (i < 0)
-		status = EXEC_ILL; /* No opening '[' */
-	    break;
+            }
+            if (i < 0)
+                status = EXEC_ILL; /* No opening '[' */
+            break;
 
-	default: 
-	    warn("jmp(%c): not a legal jmp call", text[*pc]);
-	    status = BF_ERR; 
-	    break;
+        default: 
+            warn("jmp(%c): not a legal jmp call", text[*pc]);
+            status = BF_ERR; 
+            break;
     }
 
     return status;
@@ -112,110 +167,88 @@ int
 execute(const char *text, const char *stack, char **sp, int *pc) {
     const char op = text[*pc];
     int status = 0;
+    /* Operators */
+    if (op == '>') {
+        ++*sp;
+        if ((long)(*sp - stack) + 1 > STACKSIZE) 
+            status = EXEC_MEMERR;
+    } else if (op == '<') {
+        --*sp;
+        if (*sp < stack) 
+            status = EXEC_MEMERR;
+    } else if (op == '+') {
+        ++**sp;
+    } else if (op == '-') {
+        --**sp;
+    } else if (op == '.') {
+        putchar(**sp);
+    } else if (op == ',') {
+        **sp = getchar();
+    } else if (op == '[') {
+        status = jmp(text, pc, *sp, iszero);
+    } else if (op == ']') {
+        status = jmp(text, pc, *sp, isnonzero);
+    } else if (op == '\0') {
+        status = EXEC_DONE;
+    }
+    return status;
+}
 
-    switch (op) {
-	/* Operators */
-	case '>':
-	    ++*sp;
-	    if ((long)(*sp - stack) + 1 > STACKSIZE) 
-		status = EXEC_MEMERR;
-	    break;
-	case '<':
-	    --*sp;
-	    if (*sp < stack) 
-		status = EXEC_MEMERR;
-	    break;
-	case '+':
-	    ++**sp;
-	    break;
-	case '-':
-	    --**sp;
-	    break;
-	case '.':
-	    putchar(**sp);
-	    break;
-	case ',':
-	    **sp = getchar();
-	    break;
-	case '[':
-	    status = jmp(text, pc, *sp, iszero);
-	    break;
-	case ']':
-	    status = jmp(text, pc, *sp, isnonzero);
-	    break;
+int
+run(const char *text, const char *filename) {
+    char stack[STACKSIZE] = {0};
+    char *sp = stack;
+    int pc;
+    int status = 0; 
 
-	    /* Special */
-	case '\0':
-	    status = EXEC_DONE;
-	    break;
-
-	default: 
-	    break;
+    for (pc = 0; status != EXEC_DONE; ++pc) {
+        status = execute(text, stack, &sp, &pc);
+        if (status <= -2) {
+            if (status == EXEC_ILL) {
+                warn("%s: text[%d] `%c': illegal instruction", filename, pc, text[pc]);
+                break;
+            } else if (status == BF_ERR) {
+                warn("%s: text[%d]: brainfuck interpreter error", filename, pc);
+                break;
+            } else if (status == EXEC_MEMERR) {
+                warn("%s: text[%d] `%c': stack[%ld] out of bounds", 
+                        filename, pc, text[pc], (long)(sp - stack));
+                break;
+            }
+        }
     }
 
     return status;
 }
 
 int
-run(const char *text, const char *name) {
-    char stack[STACKSIZE] = { 0 };
-    char *sp = stack;
-    int pc;
-    int status = 0; 
-
-    for (pc = 0; status != EXEC_DONE; ++pc) {
-	status = execute(text, stack, &sp, &pc);
-	if (status <= -2) {
-	    if (status == EXEC_ILL) {
-		warn("%s: text[%d] `%c': illegal instruction", name, pc, text[pc]);
-		break;
-	    } else if (status == BF_ERR) {
-		warn("%s: text[%d]: brainfuck interpreter error", name, pc);
-		break;
-	    } else if (status == EXEC_MEMERR) {
-		warn("%s: text[%d] `%c': stack[%ld] out of bounds", 
-			name, pc, text[pc], (long)(sp - stack));
-		break;
-	    }
-	}
-    }
-
+brainfuck(FILE *fin, const char *filename) {
+    int status = 0;
+    char *text = load(fin);
+    if (fclose(fin) || run(text, filename))
+        status = 1;
+    if (text)
+        free(text);
     return status;
 }
 
 int
 main(int argc, char **argv) {
     const char *filename;
-    const char *text;
     FILE *fin;
     int status = 0;
-    int ret = 0;
-
     if (argc > 1) {
-	while (--argc) {
-	    filename = *++argv;
-	    if ((fin = fopen(filename, "r")) == NULL) {
-		warn("couldn't open %s:", filename);
-		status = 1;
-		continue;
-	    }
-	    text = load(fin);
-	    fclose(fin);
-	    ret = run(text, filename);
-	    if (ret != 0) 
-		status = 1;
-	    if (text)
-		free((void *)text);
-	}
+        while (--argc) {
+            filename = *++argv;
+            if ((fin = fopen(filename, "r")) == NULL) {
+                warn("couldn't open %s:", filename);
+                status = 1;
+                continue;
+            }
+            status |= brainfuck(fin, filename);
+        }
     } else {
-	text = load(stdin);
-	filename = "stdin";
-	ret = run(text, filename);
-	if (ret != 0) 
-	    status = 1;
-	if (text) 
-	    free((void *)text);
+        status |= brainfuck(stdin, "<stdin>");
     }
-
     return status;
 }
